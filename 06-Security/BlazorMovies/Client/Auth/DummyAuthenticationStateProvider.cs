6b3104603b0763529ca3eff399e9f1f9097aaf5a
Gary Hake <Gary.Hake@waystar.com>
2020-09-19 12:07:02 -0400

    hiding ui behind auth roles

--- 06-Security/BlazorMovies/Client/Auth/DummyAuthenticationStateProvider.cs
+++ 06-Security/BlazorMovies/Client/Auth/DummyAuthenticationStateProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -8,8 +9,13 @@ namespace BlazorMovies.Client.Auth
     {
         public async override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            await Task.Delay(3000);
-            var anonymous = new ClaimsIdentity();
+            //await Task.Delay(3000);
+            var anonymous = new ClaimsIdentity(new List<Claim>()
+            {
+                new Claim("key1", "value1"),
+                new Claim(ClaimTypes.Name, "Gary"),
+                new Claim(ClaimTypes.Role, "Admin")
+            });
             return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymous)));
         }
     }
